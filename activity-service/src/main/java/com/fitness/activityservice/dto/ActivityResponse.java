837e92dc112d55dc707fb48db38b23a8f9319fa1
zhangmh123 <602409009@qq.com>
2026-01-04 15:51:00 +0800

    create activity service and integrated with mongo db.

---
+++ activity-service/src/main/java/com/fitness/activityservice/dto/ActivityResponse.java
@@ -0,0 +1,20 @@
+package com.fitness.activityservice.dto;
+
+import com.fitness.activityservice.model.ActivityType;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+public class ActivityResponse {
+    private String id;
+    private String userId;
+    private ActivityType type;
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private Map<String, Object> additionalMetrics;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ activity-service/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,85 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+    //private final UserValidationService userValidationService;
+    //private final RabbitTemplate rabbitTemplate;
+
+//    @Value("${rabbitmq.exchange.name}")
+//    private String exchange;
+//
+//    @Value("${rabbitmq.routing.key}")
+//    private String routingKey;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+
+//        boolean isValidUser = userValidationService.validateUser(request.getUserId());
+//        if (!isValidUser) {
+//            throw new RuntimeException("Invalid User: " + request.getUserId());
+//        }
+
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+
+        Activity savedActivity = activityRepository.save(activity);
+
+        // Publish to RabbitMQ for AI Processing
+//        try {
+//            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+//        } catch(Exception e) {
+//            log.error("Failed to publish activity to RabbitMQ : ", e);
+//        }
+
+        return mapToResponse(savedActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity){
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setUserId(activity.getUserId());
+        response.setType(activity.getType());
+        response.setDuration(activity.getDuration());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        return response;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> activities = activityRepository.findByUserId(userId);
+        return activities.stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(() -> new RuntimeException("Activity not found with id: " + activityId));
+    }
+}
